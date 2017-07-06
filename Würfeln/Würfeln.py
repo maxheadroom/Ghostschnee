d04d5ca866c12e160477a429674dd05140fb94ff
Falko Zurell <falko.zurell@here.com>
2017-07-06 09:10:54 +0200

    make score visible all the time

--- Würfeln/Würfeln.py
+++ Würfeln/Würfeln.py
@@ -10,16 +10,16 @@ def T1():
     GZ = randint(1,3)
     if GZ == 1:
         text.delete(0.0, END)
-        text.insert(END, 'Geist\n schnell weg!!')
-        Mut = Mut -1
+        text.insert(END, 'Score: '+str(score)+'\nGeist\n schnell weg!!')
+        Mut=Mut-1
     elif GZ == 2 or GZ == 3:
         text.delete(0.0, END)
-        text.insert(END, 'Glück gehabt\n die Luft ist rein')
+        text.insert(END, 'Score: '+str(score)+'\nGlück gehabt\n die Luft ist rein')
         Mut = Mut + 0.5
-        score= score+1
+        score=score+1
     if Mut < 0:
         text.delete(0.0, END)
-        text.insert(END, ('Der Geist hat dich erwischt\n du hast so viele Türen geschaft:',score))
+        text.insert(END, ('Score: '+str(score)+'\nDer Geist hat dich erwischt\n du hast so viele Türen geschaft:',score))
         window.update()
         time.sleep(3)
         window.quit()
@@ -32,16 +32,16 @@ def T2():
     GZ = randint(1,3)
     if GZ == 2:
         text.delete(0.0, END)
-        text.insert(END, 'Geist\n schnell weg!!')
+        text.insert(END, 'Score: '+str(score)+'\nGeist\n schnell weg!!')
         Mut = Mut -1
     elif GZ == 1 or GZ == 3:
         text.delete(0.0, END)
-        text.insert(END, 'Glück gehabt\n die Luft ist rein')
+        text.insert(END, 'Score: '+str(score)+'\nGlück gehabt\n die Luft ist rein')
         Mut = Mut + 0.5
         score= score+1
     if Mut < 0:
         text.delete(0.0, END)
-        text.insert(END, 'Der Geist hat dich erwischt\n du hast so viele Türen geschaft:',score)
+        text.insert(END, 'Score: '+str(score)+'\nDer Geist hat dich erwischt\n du hast so viele Türen geschaft:',score)
         window.update()
         time.sleep(3)
         window.quit()
@@ -51,16 +51,16 @@ def T3():
     GZ = randint(1,3)
     if GZ == 3:
         text.delete(0.0, END)
-        text.insert(END, 'Geist\n schnell weg!!')
+        text.insert(END, 'Score: '+str(score)+'\nGeist\n schnell weg!!')
         Mut = Mut -1
     elif GZ == 2 or GZ == 1:
         text.delete(0.0, END)
-        text.insert(END, 'Glück gehabt\n die Luft ist rein')
+        text.insert(END, 'Score: '+str(score)+'\nGlück gehabt\n die Luft ist rein')
         Mut = Mut + 0.5
         score= score+1
     if Mut < 0:
         text.delete(0.0, END)
-        text.insert(END, 'Der Geist hat dich erwischt\n du hast so viele Türen geschaft:',score)
+        text.insert(END, 'Score: '+str(score)+'\nDer Geist hat dich erwischt\n du hast so viele Türen geschaft:',score)
         window.update()
         time.sleep(3)
         window.quit()
@@ -71,16 +71,9 @@ b1 = Button(window, text='1', command=T1)
 b2 = Button(window, text='2', command=T2)
 b3 = Button(window, text='3', command=T3)
 text.pack()
-text.insert(END, 'Geisterspiel \n vor dir sind drei Türen \n hinter einer steht ein Geist')
+text.insert(END, 'Geisterspiel \n Score: '+str(score)+'\n vor dir sind drei Türen \n hinter einer steht ein Geist')
 window.update()
 b1.pack()
 b2.pack()
 b3.pack()
 mainloop()
-
-
-
-
-
-
-
